7eaf7c0ca28dada9c8216fbf3e303883cafabf04
Marcely Biguzzi Santello <marcely@sbsoftware.com.br>
2025-01-06 23:58:05 -0300

    Project created

---
+++ src/main/java/com/masantello/booksreviewsystem/BooksReviewSystemApplication.java
@@ -0,0 +1,13 @@
+package com.masantello.booksreviewsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksReviewSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksReviewSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/masantello/booksreviewsystem/BooksReviewSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masantello.booksreviewsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksReviewSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
