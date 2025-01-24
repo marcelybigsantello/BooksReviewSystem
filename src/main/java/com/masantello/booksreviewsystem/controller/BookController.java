66fdb41935547ece87acd252d652ad9d924d4b3c
Marcely Biguzzi Santello <marcely@sbsoftware.com.br>
2025-01-24 00:10:49 -0300

    Endpoints findAll and findOne of Book entity working

--- src/main/java/com/masantello/booksreviewsystem/controller/BookController.java
+++ src/main/java/com/masantello/booksreviewsystem/controller/BookController.java
@@ -1,19 +1,16 @@
 package com.masantello.booksreviewsystem.controller;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.masantello.booksreviewsystem.domain.Author;
 import com.masantello.booksreviewsystem.domain.Book;
-import com.masantello.booksreviewsystem.domain.enums.Genrer;
 import com.masantello.booksreviewsystem.dto.BookDTO;
 import com.masantello.booksreviewsystem.services.BookService;
 
@@ -31,14 +28,17 @@ public class BookController {
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<BookDTO>> findAll() {
-		//List<Book> books = bookService.findAll();
-		Author author = new Author("06", "George Orwell", "georgeorwellofficial@gmail.com", LocalDate.now(), Genrer.DYSTOPIAN);
-		List<Book> books = new ArrayList<>();
-		books.add(new Book("01", "A Revolução dos Bichos", "Teste", "Editora Arqueiro", (short) 90, 
-				LocalDate.of(2005, 9, 01), (float) 19.90, 100, author));
+		List<Book> books = bookService.findAll();
 		
 		List<BookDTO> booksDto = books.stream().map(book -> new BookDTO(book)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(booksDto);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<BookDTO> findOne(@PathVariable String id) {
+		Book book = bookService.findById(id);
+		
+		return ResponseEntity.ok().body(new BookDTO(book));
 	}
 
 }
