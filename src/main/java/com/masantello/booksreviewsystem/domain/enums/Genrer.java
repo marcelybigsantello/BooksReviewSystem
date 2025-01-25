ff34d83c633f723ce9616272317cc386c920b621
Marcely Biguzzi Santello <marcely@sbsoftware.com.br>
2025-01-24 22:34:53 -0300

    Ascending order of Genrer's enum

--- src/main/java/com/masantello/booksreviewsystem/domain/enums/Genrer.java
+++ src/main/java/com/masantello/booksreviewsystem/domain/enums/Genrer.java
@@ -2,26 +2,26 @@ package com.masantello.booksreviewsystem.domain.enums;
 
 public enum Genrer {
 
-	FANTASY(1, "Fantasy"),
-	SCIENCE_FICTION(2, "Science Fiction"),
-	DYSTOPIAN(3, "Dystopian"),
-	ACTION(4, "Action"),
-	BIOGRAPHY(5, "Biography"),
-	MYSTERY(6, "Mystery"),
-	ROMANCE(7, "Romance"),
-	ADVENTURE(8, "Adventure"),
-	HISTORY(9, "History"),
-	THRILLER(10, "Thriller"),
-	FOOD_AND_DRINK(11, "Food and Drink"),
-	SELF_HELP(12, "Self Help"),
-	HORROR(13, "Horror"),
-	TRUE_CRIME(14, "True Crime"),
-	HUMOR(15, "Humor"),
-	RELIGION_SPIRITUALITY(16, "Religion and Spirituality"),
-	SOCIAL_SCIENCES(17, "Social Sciences"),
-	PARENTING_FAMILIES(18, "Parenting and Families"),
-	SCIENCE_TECHNOLOGY(19, "Science and Technology"),
-	CHILDRENS(20, "Children's");
+	ACTION(1, "Action"),
+	ADVENTURE(2, "Adventure"),
+	BIOGRAPHY(3, "Biography"),
+	CHILDRENS(4, "Children's"),
+	FANTASY(5, "Fantasy"),
+	DYSTOPIAN(6, "Dystopian"),
+	FOOD_AND_DRINK(7, "Food and Drink"),
+	HISTORY(8, "History"),
+	HORROR(9, "Horror"),
+	HUMOR(10, "Humor"),
+	MYSTERY(11, "Mystery"),
+	PARENTING_FAMILIES(12, "Parenting and Families"),
+	RELIGION_SPIRITUALITY(13, "Religion and Spirituality"),
+	ROMANCE(14, "Romance"),
+	SCIENCE_FICTION(15, "Science Fiction"),
+	SCIENCE_TECHNOLOGY(16, "Science and Technology"),
+	SELF_HELP(17, "Self Help"),
+	SOCIAL_SCIENCES(18, "Social Sciences"),
+	THRILLER(19, "Thriller"),
+	TRUE_CRIME(20, "True Crime");
 	
 	private Integer code;
 	private String description;
